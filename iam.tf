c9a5f260b190c2eb0767bd3cb46d83e4b7cdf92d
Natanael <taelima1997@gmail.com>
2024-06-29 09:21:02 -0300

    chore: Update IAM role permissions for Terraform

--- iam.tf
+++ iam.tf
@@ -58,6 +58,24 @@ resource "aws_iam_role" "tf-role" {
     ]
   })
 
+   inline_policy {
+    name = "tf-permissions"
+    policy = jsonencode({
+      Statement = [{
+        Sid      = "Statement1"
+        Action   = "ecr:*"
+        Effect   = "Allow"
+        Resource = "*"
+        },
+        {
+          Sid = "Statement2"
+          Action = "iam:*"
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
   tags = {
     IAC = "True"
   }
