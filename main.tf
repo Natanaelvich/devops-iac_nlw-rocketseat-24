6d434c3af2d81f9f74a88a9dce8bde703844e955
Natanael <taelima1997@gmail.com>
2024-06-30 10:22:48 -0300

    chore: Add backend configuration for Terraform state storage in S3

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.49.0"
     }
   }
+
+
+  backend "s3" {
+    bucket = "natanaelvich-iac"
+    key    = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
 
 provider "aws" {
